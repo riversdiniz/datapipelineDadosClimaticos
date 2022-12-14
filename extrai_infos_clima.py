76e28b6740d47c3669ad254be5f57dd1f44e5b80
River Diniz <river-s-diniz@hotmail.com>
2022-12-14 11:32:35 -0300

    Adicionando comentario.

--- extrai_infos_clima.py
+++ extrai_infos_clima.py
@@ -7,22 +7,26 @@ from datetime import datetime, timedelta
 data_inicio = datetime.today()
 data_fim = data_inicio + timedelta(days=7)
 
-# formatando as datas
+# Definir o intervalo de tempo, cidade e a chave
 data_inicio = data_inicio.strftime('%Y-%m-%d')
 data_fim = data_fim.strftime('%Y-%m-%d')
 
 city = 'Manaus'
 key = 'Y9WTLKNYPSUJH6VFYJEALZMAS'
 
+# Passar as informações para a URL
 URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
             f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
 
+# Extrair os dados em csv e printar as primeiras linhas no terminal
 dados = pd.read_csv(URL)
 print(dados.head())
 
+# Definir o diretório para salvar os arquivos e criar uma pasta
 file_path = f'/home/riverd/datapipeline/semana={data_inicio}/'
 os.mkdir(file_path)
 
+# Salvar os arquivos em csv
 dados.to_csv(file_path + 'dados_brutos.csv')
 dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
 dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
